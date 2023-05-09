77d7dcb6b786747274e72d6896fcb2b5880e5ba1
Yashas <yashasjraj@gmail.com>
2023-05-09 15:11:06 +0530

    initial commit

---
+++ client/src/utils/constants.js
@@ -0,0 +1,4 @@
+import abi from "./Transactions.json";
+
+export const contractAddress = "0x149cD14E8E6461A38cf4eF6a8aA741d7FD8e83e0";
+export const contractABI = abi.abi;
---
+++ smart_contract/hardhat.config.js
@@ -0,0 +1,16 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+module.exports = {
+  solidity: "0.8.0",
+  networks: {
+    sepolia: {
+      url: "https://eth-sepolia.g.alchemy.com/v2/n1w3IwUtKR5hWDuMTgrQRWWC8-Z-4o0K",
+      accounts: [
+        "e45afd9c0399137e8a8539c25b29c71c8e21dc7b8f8a87c6d2e29a12b86ea3ea",
+      ],
+    },
+  },
+};
+
+// smart contract  transactions address
+// 0x149cD14E8E6461A38cf4eF6a8aA741d7FD8e83e0
